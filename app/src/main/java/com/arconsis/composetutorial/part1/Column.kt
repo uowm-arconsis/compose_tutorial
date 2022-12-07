052dfa787675e46df1f6c8d0feebba641014eec6
Alexandros Koufatzis <alexandros.koufatzis@arconsis.com>
2022-12-07 22:05:31 +0200

    Add one example for the studens with a greek comment description

--- app/src/main/java/com/arconsis/composetutorial/part1/Column.kt
+++ app/src/main/java/com/arconsis/composetutorial/part1/Column.kt
@@ -12,7 +12,19 @@ fun ColumnExample() {
         Text(text = "Hello Grevena")
         Text(text = "Hello Students")
     }
+}
 
+/**
+ * Αυτό ειναι το παράδειγμα για τους μαθητές
+ * Θα πρέπει να βγάλετε τα σχόλια απο τον παρακάτω κώδικα
+ * για να δείτε το αποτέλεσμα
+ */
+@Composable
+fun ColumnExampleStudents() {
+//    Column {
+//        Text(text = "Hello Grevena")
+//        Text(text = "Hello Students")
+//    }
 }
 
 @Preview(showBackground = true)

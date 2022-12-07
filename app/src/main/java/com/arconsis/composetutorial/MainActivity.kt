b7411ee3c8a5f772b3f97b87f38cf6e7158b30af
Alexandros Koufatzis <alexandros.koufatzis@arconsis.com>
2022-12-07 21:58:09 +0200

    Optimize imports

--- app/src/main/java/com/arconsis/composetutorial/MainActivity.kt
+++ app/src/main/java/com/arconsis/composetutorial/MainActivity.kt
@@ -3,20 +3,13 @@ package com.arconsis.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Card
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.arconsis.composetutorial.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
